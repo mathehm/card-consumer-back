9027b9d13963df3404f8b1c2b9bfad530e418de8
Matheus Martins <matheushmar@gmail.com>
2024-06-25 19:34:14 -0300

    Deleta o app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
 
 @Module({
   imports: [WalletModule],
   controllers: [],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule { }
